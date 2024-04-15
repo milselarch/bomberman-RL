55a542c957191cb27a673c1b9737d7c0531da9bb
milselarch <milselarch@gmail.com>
2024-04-16 00:16:31 +0800

    set prioritized replay memory priority based on q_value error

--- TrainingSettings.py
+++ TrainingSettings.py
@@ -1,5 +1,5 @@
 class TrainingSettings(object):
     IS_MANUAL_CONTROL = False
-    IS_CHECKING_ILLEGAL_ACTION = False
+    IS_CHECKING_ILLEGAL_ACTION = True
     IS_PRESET_GRID = False
     POOL_TRANSITIONS = True
